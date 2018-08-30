ae28dbfa75a8180b7733c3a1da8d442b13e481f7
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2018-08-30 19:50:04 +0200

    Commit inicial

---
+++ src/gestorficheroserializado/MiObjectOutputStream.java
@@ -0,0 +1,30 @@
+package gestorficheroserializado;
+
+import java.io.*;
+
+/**
+ * @author DiscoDurodeRoer
+ */
+public class MiObjectOutputStream extends ObjectOutputStream{
+    
+    /** 
+     * Constructor que recibe OutputStream 
+     * @param out OutputStream
+     * @throws java.io.IOException IOException
+     */
+    public MiObjectOutputStream(OutputStream out) throws IOException{
+        super(out); //Llama al constructor de la superclase(OutputStream)
+    }
+    /** 
+     * Constructor sin parámetros 
+     * @throws java.io.IOException IOException
+     */
+    protected MiObjectOutputStream() throws IOException, SecurityException{
+        super(); //Llama al constructor de la superclase(OutputStream)
+    }
+    
+    @Override
+    protected void writeStreamHeader() throws IOException{
+        //No hacemos nada
+    }
+}
